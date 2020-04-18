c3ffac41345dc5da3fe4421b2c15e649b01944bc
Anton <warnes23@yandex.ru>
2020-04-18 14:21:46 +0300

    starter

--- src/constants/nodes.js
+++
@@ -1,2 +0,0 @@
-export const NODE_ROOT = document.getElementById('root');
-export const NODE_PAGES = document.getElementById('pages');
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import App from './components/App';
-import { NODE_ROOT } from './constants/nodes';
-
-const app = new App();
-
-NODE_ROOT.innerHTML = app.render();
